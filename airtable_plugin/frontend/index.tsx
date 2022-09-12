ab1e8d09016fd69520b51cfb45ac327da839700f
Carl Lippert <carl.lippert@gmail.com>
2022-09-12 19:44:44 +0100

    airtable plugin wagmi.

--- airtable_plugin/frontend/index.tsx
+++ airtable_plugin/frontend/index.tsx
@@ -1,9 +1,52 @@
-import {initializeBlock} from '@airtable/blocks/ui';
-import React from 'react';
+import { initializeBlock } from "@airtable/blocks/ui";
+import React from "react";
+import { Chain } from "wagmi";
+import "@rainbow-me/rainbowkit/styles.css";
+
+import {
+  getDefaultWallets,
+  RainbowKitProvider,
+  ConnectButton,
+} from "@rainbow-me/rainbowkit";
+import { chain, configureChains, createClient, WagmiConfig } from "wagmi";
+import { infuraProvider } from "wagmi/providers/infura";
+import { publicProvider } from "wagmi/providers/public";
+
+const AURORA_TESTNET: Chain = {
+  ...chain.localhost,
+  id: 1313161555,
+  name: "Aurora Testnet",
+};
+
+const { chains, provider } = configureChains(
+  [AURORA_TESTNET],
+  [
+    // infuraProvider({ apiKey: process.env.INFURA_ID }),
+    publicProvider(),
+  ]
+);
+
+const { connectors } = getDefaultWallets({
+  appName: "Nearly Finished",
+  chains,
+});
+
+const wagmiClient = createClient({
+  autoConnect: true,
+  connectors,
+  provider,
+});
 
 function HelloWorldTypescriptApp() {
-    // YOUR CODE GOES HERE
-    return <div>Hello world 🚀</div>;
+  // YOUR CODE GOES HERE
+  return (
+    <WagmiConfig client={wagmiClient}>
+      <RainbowKitProvider chains={chains}>
+        <div>Hello world 🚀</div>
+        <ConnectButton />
+      </RainbowKitProvider>
+    </WagmiConfig>
+  );
 }
 
 initializeBlock(() => <HelloWorldTypescriptApp />);
