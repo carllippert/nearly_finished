9d29c0b76b35ee287926b7d1f6d887c5373758a0
Michael Teixeira <meixeira@gmail.com>
2022-09-12 19:58:47 +0100

    Configure Chain w/ Aurora testnet

--- web2/pages/_app.tsx
+++ web2/pages/_app.tsx
@@ -1,15 +1,33 @@
 import '../styles/globals.css';
 import '@rainbow-me/rainbowkit/styles.css';
 import { getDefaultWallets, RainbowKitProvider } from '@rainbow-me/rainbowkit';
-import { chain, configureChains, createClient, WagmiConfig } from 'wagmi';
+import { chain, configureChains, createClient, WagmiConfig, Chain } from 'wagmi';
 import { jsonRpcProvider } from 'wagmi/providers/jsonRpc';
 import { publicProvider } from 'wagmi/providers/public';
 import type { AppProps } from 'next/app';
 
+const auroraChain: Chain = {
+  id: 1313161555,
+  name: 'Aurora Testnet',
+  network: 'aurora',
+  nativeCurrency: {
+    decimals: 18,
+    name: 'Ethereum',
+    symbol: 'ETH',
+  },
+  rpcUrls: {
+    default: 'https://testnet.aurora.dev/',
+  },
+  blockExplorers: {
+    default: { name: 'Aurorascan', url: 'https://testnet.aurorascan.dev/' },
+  },
+  testnet: true,
+}
+
 const { chains, provider } = configureChains(
-  [chain.mainnet],
+  [auroraChain],
   [
-    jsonRpcProvider({ rpc: () => ({ http: 'https://rpc.ankr.com/eth' }) }),
+    jsonRpcProvider({ rpc: () => ({ http: 'https://testnet.aurora.dev/' }) }),
     publicProvider(),
   ]
 );
