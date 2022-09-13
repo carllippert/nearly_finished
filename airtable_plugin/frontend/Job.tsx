12ba934d6709abbfa3ca8248447b1554c36a67fd
Carl Lippert <carl.lippert@gmail.com>
2022-09-13 08:55:53 +0100

    More airtabler stuff need abi '

--- airtable_plugin/frontend/Job.tsx
+++ airtable_plugin/frontend/Job.tsx
@@ -1,4 +1,9 @@
 import React, { useEffect, useState } from "react";
+import { usePrepareContractWrite, useContractWrite } from "wagmi";
+
+//TODO: get abi. 
+const abi = ""; 
+const address = ""; 
 
 export const JobItem = ({ record }) => {
   const [hydrated, setHydrated] = useState(false);
@@ -14,6 +19,17 @@ export const JobItem = ({ record }) => {
       alert("Changed Record");
     }
   }, [record.getCellValue("stateString")]);
+
+  const { config } = usePrepareContractWrite({
+    addressOrName: address,
+    contractInterface: abi, 
+    functionName: 'mintTo',
+  })
+  const { data, isLoading, isSuccess, write } = useContractWrite(config)
+
+  const mint = () => {
+   
+  }
 
   return <div>{record.name}</div>;
 };
