95c0f46a07f336f80dd6532a8f504ba9dd70bd97
Carl Lippert <carl.lippert@gmail.com>
2022-09-14 00:16:52 +0100

    claimed.

--- web2/components/jobCard.tsx
+++ web2/components/jobCard.tsx
@@ -12,6 +12,9 @@ export const zeroAddress = "0x0000000000000000000000000000000000000000";
 
 export type Job = {
   tokenID: string;
+  name: string; 
+  recrodId: string; 
+  hasAllowlist: boolean; 
   creator: string;
   recipient: string;
   executorFee: string;
@@ -39,6 +42,9 @@ const JobCard = ({ tokenID }: { tokenID: string }) => {
   let [job, setJob] = useState<Job>({
     tokenID,
     creator: "",
+    name: "", 
+    recordId: "", 
+    hasAllowList: "", 
     recipient: "",
     executorFee: "",
     creatorFee: "",
@@ -93,37 +99,33 @@ const JobCard = ({ tokenID }: { tokenID: string }) => {
   //   }
   // }, [job.tokenURI]);
 
-  // useEffect(() => {
-  //   if (jobStatusData && jobData) {  
-  //       let newJob: Job = {
-  //           ...job,
-  //           creator: jobData[0],
-  //           recipient: jobData[1],
-  //           executorFee: formatEther(jobData[2]),
-  //           creatorFee: formatEther(jobData[3]),
-  //           recruiterFee: formatEther(jobData[4]),
-  //           deadline: jobData[5].toNumber(),
-  //           tokenURI: jobData[6],
-  //           creatorConfirmsCompletion: jobData[7],
-  //           recipientConfirmsCompletion: jobData[8],
-  //         };
-        
-  //     setJob({
-  //       ...newJob,
-  //       claimer: jobStatusData[0],
-  //       canceller: jobStatusData[1],
-  //       recruiter: jobStatusData[2],
-  //       executer: jobStatusData[3],
-  //     });
-  //     if (jobStatusData[1] !== zeroAddress) {
-  //       //job has been cancelled by nonzeroaddress
-  //       setCancelled(true);
-  //     }
-  //     if (jobStatusData[3] !== zeroAddress) {
-  //       setFinished(true);
-  //     }
-  //   }
-  // }, [jobData, jobStatusData]);
+
+
+  useEffect(() => {
+    if(jobStatusData && jobData){
+    console.log("JobStatusData => " + JSON.stringify(jobStatusData)); 
+
+    setJob({ 
+      ...job, 
+      claimer: jobStatusData[0], 
+      canceller: jobStatusData[1],
+      recruiter: jobStatusData[2],
+      executer: jobStatusData[3],
+      name: jobData[0], 
+      recordId: jobData[1], 
+      hasAllowList: jobData[2], 
+    }) 
+ 
+    if (jobStatusData[1] !== zeroAddress) {
+            //job has been cancelled by nonzeroaddress
+            setCancelled(true);
+          } 
+          if (jobStatusData[3] !== zeroAddress) {
+            setFinished(true);
+          }
+  } 
+  }, [jobStatusData, jobData]); 
+ 
 
   return (
     <li className={`md:flex md:items-center md:justify-between py-8 px-8 my-2 rounded-lg border-2 ${finished ? "border-8 border-green-400" : ""}`} >
@@ -131,8 +133,9 @@ const JobCard = ({ tokenID }: { tokenID: string }) => {
         <h2 className="text-2xl font-bold leading-7 text-gray-900 sm:truncate sm:text-3xl sm:tracking-tight">
             Job Number: {tokenID}
         </h2>
+        {JSON.stringify(job, null, 3)}
         </div>
-        <div className="mt-4 flex md:mt-0 md:ml-4">
+        <div className="mt-4 flex md:mt-0 md:ml-4 bg-pink-100">
         <ClaimButton job={job} />
         <FinishButton job={job} />
         </div>
