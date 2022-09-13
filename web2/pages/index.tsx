fe99fb8e719f44f326fe6a33423aeb68ebb06174
Swapneel Bobba <swapneel@mimicry.finance>
2022-09-13 22:21:58 +0100

    minor tweaks

--- web2/pages/index.tsx
+++ web2/pages/index.tsx
@@ -1,7 +1,7 @@
 import type { NextPage } from "next";
 import type { FC } from "react";
 import { ConnectButton } from "@rainbow-me/rainbowkit";
-import JobItem from "./components/JobItem/JobItem";
+import JobItem, { JobProps } from "./components/JobItem/JobItem";
 
 // TODO: kill this
 const MOCK_JOBS = [
@@ -34,8 +34,8 @@ const Home: NextPage = () => {
 
       <h1 className="text-4xl font-bold mt-6 mb-6">🚀 create-web3-frontend</h1>
       <div className="flex-col space-y-3">
-        {MOCK_JOBS.map((job) => (
-          <JobItem {...job} />
+        {MOCK_JOBS.map((job: JobProps, idx) => (
+          <JobItem {...job} key={idx} />
         ))}
       </div>
     </div>
