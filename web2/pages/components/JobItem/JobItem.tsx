8fb3777c834c87f226fa95d0cb7af79d386bc79c
Swapneel Bobba <swapneel@mimicry.finance>
2022-09-13 22:09:14 +0100

    add job cards to home

---
+++ web2/pages/components/JobItem/JobItem.tsx
@@ -0,0 +1,52 @@
+import React, { FC } from "react";
+
+type Props = {
+  name: string;
+  creator: string;
+  recipient: string;
+  status: string;
+};
+
+const JobItem: FC<Props> = ({ name, creator, recipient, status }) => {
+  return (
+    <div className="p-6 max-w-lg mx-auto bg-white rounded-lg border border-gray-200 shadow-md dark:border-gray-700">
+      <a href="#">
+        <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 light:text-white">
+          {name}
+        </h5>
+      </a>
+      <div className="flex-row">
+        <p className="mb-3 font-normal text-gray-900 dark:text-gray-400">
+          {creator}
+        </p>
+        <p className="mb-3 font-normal text-gray-900 dark:text-gray-400">
+          {recipient}
+        </p>
+        <p className="mb-3 font-semibold text-sky-500 bg-sky-100 w-1/4 mx-auto rounded-md">
+          {status}
+        </p>
+      </div>
+      <a
+        href="#"
+        className="inline-flex items-center py-2 px-3 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+      >
+        Claim Job
+        <svg
+          aria-hidden="true"
+          className="ml-2 -mr-1 w-4 h-4"
+          fill="currentColor"
+          viewBox="0 0 20 20"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            fill-rule="evenodd"
+            d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+            clip-rule="evenodd"
+          ></path>
+        </svg>
+      </a>
+    </div>
+  );
+};
+
+export default JobItem;
--- web2/pages/index.tsx
+++ web2/pages/index.tsx
@@ -1,15 +1,43 @@
-import type { NextPage } from 'next';
-import type { FC } from 'react';
-import { ConnectButton } from '@rainbow-me/rainbowkit';
+import type { NextPage } from "next";
+import type { FC } from "react";
+import { ConnectButton } from "@rainbow-me/rainbowkit";
+import JobItem from "./components/JobItem/JobItem";
+
+// TODO: kill this
+const MOCK_JOBS = [
+  {
+    name: "This is a Job Name",
+    creator: "Creator Name",
+    recipient: "Recipient Name",
+    status: "Completed",
+  },
+  {
+    name: "This is a Job Name",
+    creator: "Creator Name",
+    recipient: "Recipient Name",
+    status: "Completed",
+  },
+  {
+    name: "This is a Job Name",
+    creator: "Creator Name",
+    recipient: "Recipient Name",
+    status: "Completed",
+  },
+];
 
 const Home: NextPage = () => {
   return (
-    <div className='py-6 justify-center text-center'>
-      <div className='flex justify-center'>
+    <div className="py-6 justify-center text-center">
+      <div className="flex justify-center">
         <ConnectButton />
       </div>
 
-      <h1 className='text-4xl font-bold mt-6'>🚀 create-web3-frontend</h1>
+      <h1 className="text-4xl font-bold mt-6 mb-6">🚀 create-web3-frontend</h1>
+      <div className="flex-col space-y-3">
+        {MOCK_JOBS.map((job) => (
+          <JobItem {...job} />
+        ))}
+      </div>
       <InfoSection />
     </div>
   );
@@ -17,27 +45,27 @@ const Home: NextPage = () => {
 
 const InfoSection: FC = () => {
   return (
-    <div className='mt-10'>
-      <h2 className='text-xl font-bold'>If you need help</h2>
-      <div className='flex flex-col gap-2 mt-2'>
+    <div className="mt-10">
+      <h2 className="text-xl font-bold">If you need help</h2>
+      <div className="flex flex-col gap-2 mt-2">
         <a
-          href='https://wagmi.sh'
-          target='_blank'
-          className='underline text-gray-600'
+          href="https://wagmi.sh"
+          target="_blank"
+          className="underline text-gray-600"
         >
           Link to wagmi docs
         </a>
         <a
-          href='https://github.com/dhaiwat10/create-web3-frontend'
-          target='_blank'
-          className='underline text-gray-600'
+          href="https://github.com/dhaiwat10/create-web3-frontend"
+          target="_blank"
+          className="underline text-gray-600"
         >
           Open an issue on Github
         </a>
         <a
-          href='https://twitter.com/dhaiwat10'
-          target='_blank'
-          className='underline text-gray-600'
+          href="https://twitter.com/dhaiwat10"
+          target="_blank"
+          className="underline text-gray-600"
         >
           DM me on Twitter
         </a>
